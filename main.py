b4faea1d2c64d211125306f08fefbb5f8433d714
Leandro Spinosi <leanspino@gmail.com>
2026-08-24 12:06:04 +0200

    implementation classe d'animaux et liste d'animaux

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+from models.animal import Animal
 from models.personne import Personne
 
 
 p1 = Personne("spinosi","leandro")
+a1 = Animal("vache")
+
+p1.aj_ani(a1)
--- models/personne.py
+++ models/personne.py
@@ -1,4 +1,11 @@
+from models.animal import Animal
+
+
 class Personne:
     def __init__(self,nom,personne) -> None:
         self.nom= nom
         self.personne = personne
+        self.animaux = []
+
+    def aj_ani(self,animal : Animal):
+        self.animaux.append(animal)
