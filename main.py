3ad5c697bb065fc8b9f1331866c212fe8e2c00da
Leandro Spinosi <leanspino@gmail.com>
2026-08-24 11:47:25 +0200

    commit Initial

---
+++ main.py
@@ -0,0 +1,4 @@
+from models.personne import Personne
+
+
+p1 = Personne("spinosi","leandro")
---
+++ models/personne.py
@@ -0,0 +1,4 @@
+class Personne:
+    def __init__(self,nom,personne) -> None:
+        self.nom= nom
+        self.personne = personne
